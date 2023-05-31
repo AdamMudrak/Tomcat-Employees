09f639dd12ae55b03d1003b79129ea3243c05781
AdamMudrak <mudrets2000@gmail.com>
2023-05-31 20:15:04 +0300

    Add other DB authorization data

---
+++ src/main/java/org/example/app/utils/Constants.java
@@ -0,0 +1,10 @@
+package org.example.app.utils;
+
+import org.example.app.credentials.Credentials;
+
+public class Constants {
+
+    public final static String DB_URL = "jdbc:mysql://localhost:3306/employees_db?";
+    public final static String DB_USER = "root";
+    public final static String DB_PASS = Credentials.DB_PASS;
+}
